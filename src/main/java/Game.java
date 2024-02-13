19bda4a7b3e4a208b892d7749c4c38c41b1f5390
MeadowReiber <1108718@apps.nsd.org>
2024-02-13 13:53:34 -0800

    first commit

---
+++ src/main/java/Game.java
@@ -0,0 +1,13 @@
+public class Game{
+    //fields and properties----------------------
+    private int[][] board;
+    private Player[] players;
+    
+    //constructor--------------------------------
+    public Game(Player x, Player o){
+        players = new Player[] {x, o};
+        
+    }
+
+    //methods------------------------------------
+}
